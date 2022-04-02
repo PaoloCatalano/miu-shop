714700aec606fed723d8291a802fcd78f7e615c3
Paolo Catalano <paolo12587@gmail.com>
2022-04-02 20:25:39 +0200

    ▲

--- components/Footer.js
+++ components/Footer.js
@@ -7,11 +7,17 @@ const Footer = () => {
       <div className="_lg">
         Copyright &copy; {new Date().getFullYear()}{" "}
         <span className="_info-bold">{process.env.WEBSITE_NAME} </span>
-        All Rights Reserved.
+        All Rights Reserved. Powered by{" "}
+        <a href="http://vercel.com" target="_blank" rel="noopener noreferrer">
+          ▲
+        </a>
       </div>
       <div className="_sm">
         <span className="_info-bold">{process.env.WEBSITE_NAME} </span>
-        &copy; {new Date().getFullYear()}
+        &copy; {new Date().getFullYear()}{" "}
+        <a href="http://vercel.com" target="_blank" rel="noopener noreferrer">
+          ▲
+        </a>
       </div>
       <Socials />
     </footer>
